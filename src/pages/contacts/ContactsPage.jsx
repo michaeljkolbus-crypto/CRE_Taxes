e3ad54cc22e6babac320ec45cb5a4bbd1999a9f4
michaeljkolbus-crypto <michaeljkolbus@gmail.com>
2026-04-16 10:11:36 -0500

    Initial commit: scaffold + schema

---
+++ src/pages/contacts/ContactsPage.jsx
@@ -0,0 +1,3 @@
+export default function ContactsPage() {
+  return <div style={{ padding: 24 }}><h2 style={{ fontSize: 20, fontWeight: 700, color: '#1e293b', marginBottom: 8 }}>Contacts</h2><p style={{ color: '#64748b' }}>Contacts list coming soon.</p></div>
+}
